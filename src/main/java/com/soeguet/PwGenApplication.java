08802cb85c407104840b4a4eb84c3991c0b765ff
soeguet <osman_s@live.de>
2023-08-29 10:10:54 +0200

    init

---
+++ src/main/java/com/soeguet/PwGenApplication.java
@@ -0,0 +1,14 @@
+package com.soeguet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwGenApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PwGenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soeguet/PwGenApplicationTests.java
@@ -0,0 +1,14 @@
+package com.soeguet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwGenApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
